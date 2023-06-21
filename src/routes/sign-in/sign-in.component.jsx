5659abf94656ab6f9f0f2ebf0527e3749164f77f
Emmanuel <emmanuelolawuni2001@gmail.com>
2023-06-21 17:18:18 +0100

    Firebase DocRef

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import { signInWithGooglePopup } from '../../../src/utils/firebase/firebase.utils'
+import { signInWithGooglePopup , CreateUserDocumentfromAuth} from '../../../src/utils/firebase/firebase.utils'
 
 const SignIn = () => {
     const logGoogleUser = async ()=> {
-        const response = await signInWithGooglePopup();
-        console.log(response);
+        const {user } = await signInWithGooglePopup();
+       const userDocRef = await  CreateUserDocumentfromAuth(user)
     }
     return (
         <div> <h1> Sign In Page</h1> 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,21 +1,48 @@
-import {initializeApp} from 'firebase/app'
-import {getAuth , signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAj6shbtHe42iQtn6XQZzE-HypAZ7cuK4g",
-    authDomain: "crown-clothing-emma.firebaseapp.com",
-    projectId: "crown-clothing-emma",
-    storageBucket: "crown-clothing-emma.appspot.com",
-    messagingSenderId: "799122598636",
-    appId: "1:799122598636:web:6d3a31a8f4dfb17dc0d3c9"
-  };
-  
-  // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: 'select_account'
-  })
+  apiKey: "AIzaSyAj6shbtHe42iQtn6XQZzE-HypAZ7cuK4g",
+  authDomain: "crown-clothing-emma.firebaseapp.com",
+  projectId: "crown-clothing-emma",
+  storageBucket: "crown-clothing-emma.appspot.com",
+  messagingSenderId: "799122598636",
+  appId: "1:799122598636:web:6d3a31a8f4dfb17dc0d3c9"
+};
 
-  export const auth = getAuth()
-  export const signInWithGooglePopup = () => signInWithPopup(auth , provider);
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account'
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const CreateUserDocumentfromAuth = async(UserAuth)=>{
+  const userDocRef = doc(db , 'users' , UserAuth.uid)
+  console.log(userDocRef);
+
+  const userSnapshot = getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log((await userSnapshot).exists());
+  //If user does not exists
+  if(!userSnapshot.exists()){
+    const  {displayName , email } = UserAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef , {
+        displayName, email , createdAt,
+      })
+    } catch (error) {
+      console.log('Error leleyi', error.message);
+    }
+    return userDocRef;
+  }
+};
+
+
+
